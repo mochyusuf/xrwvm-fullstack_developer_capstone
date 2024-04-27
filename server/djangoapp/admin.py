f62996461ad32983ca4c358a8c2c8c400f8fd184
Mochamad Yusuf <mochyusuf100@gmail.com>
2024-04-27 19:54:27 +0700

    CarMake, CarModel

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,6 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
